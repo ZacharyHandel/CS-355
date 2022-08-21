7a6d0db103fdc9f63c3c7aa2f2e9e06162dd7b34
Zachary <zachary.handelzh@gmail.com>
2022-08-20 22:36:32 -0500

    New Number Feature

--- TestCode.cpp
+++ TestCode.cpp
@@ -4,9 +4,15 @@ using namespace std;
 int main()
 {
     string name;
+    int favNum;
     cout << "Hello! What is your name?" << endl;
     cin >> name;
     cout << "Hello, " << name << endl;
     
+    cout << "What is your favorite number? I will add 3 to it!" << endl;
+    cin >> favNum;
+    favNum += 3;
+    cout << "the new number is now " << favNum << endl;
+
     return 0;
 }

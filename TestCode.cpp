9e917e8d394c8738711b8e485535667cb1ee765a
Zachary <zachary.handelzh@gmail.com>
2022-08-20 22:47:42 -0500

    New IF Feature

--- TestCode.cpp
+++ TestCode.cpp
@@ -14,5 +14,21 @@ int main()
     favNum += 3;
     cout << "the new number is now " << favNum << endl;
 
+    cout << "Input either 1 or 2. If you say 1, I will say yay! if you say 2, I will say boo!" << endl;
+    int choice;
+
+    if(choice == 1)
+    {
+        cout << "YAY!" << endl;
+    }
+    else if(choice == 2)
+    {
+        cout << "BOO!" << endl;
+    }
+    else
+    {
+        cout << "You did not even input one of the choices I suggested... Goodbye!" << endl;
+    }
+
     return 0;
 }

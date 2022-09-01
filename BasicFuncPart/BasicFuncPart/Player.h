b833d6c20d9428cb404e0a0bc621668aaf99f381
Zachary Handel <zachary.handelzh@gmail.com>
2022-08-31 21:01:35 -0500

    Game Status Updator (detect if win or loss)

--- BasicFuncPart/BasicFuncPart/Player.h
+++ BasicFuncPart/BasicFuncPart/Player.h
@@ -1,6 +1,6 @@
 class Player{
   public:
-    Player(){
+    Player(){	//constructor
 		lastLocation = NULL;
 		currentLocation = NULL;
 	}
@@ -13,13 +13,29 @@ class Player{
 	bool playerMoved(){
 		return !(lastLocation == currentLocation);
 	}
+
 	areaNode* getCurrent(){
 		return currentLocation;
 	}
 
 	//isGameOver should return: 0 for continue, 1 for win, 2 for lose
 	int isGameOver(){
-		 return 0;
+		 if(currentLocation->info.getID() == 1)	//here we are locating the info object of Area created in the areaNode struct. By this, we are able to access the values regarding getID and getGoal.
+		 {
+			return 2;
+		 }
+		 else if(currentLocation->info.getGoal() == 1)
+		 {
+			return 1;
+		 }
+		 else if(currentLocation->info.getID() == 0 && currentLocation->info.getGoal() == 0)
+		 {
+			return 0;
+		 }
+		 else
+		 {
+			return 0;
+		 }
 	}
 
   private:
--- BasicFuncPart/BasicFuncPart/mainIFD.cpp
+++ BasicFuncPart/BasicFuncPart/mainIFD.cpp
@@ -1,3 +1,10 @@
+/*
+Name- Zachary Handel
+Assignment- IFD Game Engine
+Due Date- *There will be many changes to this program*
+Description- This is the main driver for the IFD game engine created by Dr. Ray. Throughout this course, we will be making changes and adding
+			 new features to learn new concepts of OOP.
+*/
 #include "Game.h"
 
 using namespace std;

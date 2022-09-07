dcdc62562552740b47061c964cb120caf9973914
Zachary Handel <zachary.handelzh@gmail.com>
2022-09-07 15:03:51 -0500

    Creating default constructor and constructor

--- CaesarCipher/advancedCaesar.h
+++ CaesarCipher/advancedCaesar.h
@@ -1,7 +1,60 @@
 #include <iostream>
 #include "caesarCipher.h"
+using namespace std;
 
+int const MAX_ALPHABET_SIZE = 26;
 class advancedCaesar: public caesarCipher
 {
-    
+    private:
+        char cipherAlphabet[MAX_ALPHABET_SIZE] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+    public:
+        advancedCaesar()
+        {
+                shiftValue = 1;
+
+                for(int i = 0; i < MAX_ALPHABET_SIZE; i++)
+                {
+                    if(cipherAlphabet[i] >= 97 && cipherAlphabet[i] <= 122)
+                    {
+                        if(cipherAlphabet[i]+shiftValue > 122)
+                        {
+                            cipherAlphabet[i] = 122 - (cipherAlphabet[i]%97);
+                        }
+                        else
+                        {
+                            cipherAlphabet[i] += shiftValue;
+                        }
+                    }
+                }
+        }
+
+        advancedCaesar(int value)
+        {
+            shiftValue = value;
+
+            for(int i = 0; i < MAX_ALPHABET_SIZE; i++)
+                {
+                    if(cipherAlphabet[i] >= 97 && cipherAlphabet[i] <= 122)
+                    {
+                        if(cipherAlphabet[i]+shiftValue > 122)
+                        {
+                            cipherAlphabet[i] = 122 - (cipherAlphabet[i]%97);
+                        }
+                        else
+                        {
+                            cipherAlphabet[i] += shiftValue;
+                        }
+                    }
+                }
+        }
+
+        void print()
+        {
+            cout << "Shift Value: " << shiftValue << endl;
+            for(int i = 0; i < MAX_ALPHABET_SIZE; i++)
+            {
+                cout << "New Cipher Alphabet: " << cipherAlphabet[i] << endl;
+            }
+        }
 };

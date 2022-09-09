80b6ef787187460388bf632c28b6548a8990efcc
Zachary Handel <zachary.handelzh@gmail.com>
2022-09-08 21:31:19 -0500

    Creating members of item.h

--- BasicFuncPart/Item.h
+++ BasicFuncPart/Item.h
@@ -8,44 +8,51 @@ class Item
         string itemDescription;
         int srItems;
     public:
-        void setName(string)
+        void setName(string name)
         {
-
+            itemName = name;
         }
 
-        void setDesc(string)
+        void setDesc(string desc)
         {
-
+            itemDescription = desc;
         }
 
-        void setSR(int)
+        void setSR(int sr)
         {
-
+            srItems = sr;
         }
 
         string getName()
         {
-
+            return itemName;
         }
 
         string getDesc()
         {
-
+            return itemDescription;
         }
 
         int getSR()
-        {
-
+        {  
+            return srItems;
         }
 
         void displayName()
         {
-
+            cout << itemName << endl;
         }
 
         void displayDesc()
         {
-            
+            cout << itemDescription << endl;
+        }
+
+        friend ostream& operator << (ostream& output, Item& item)
+        {
+            output << item.itemName << endl;
+            output << item.itemDescription << endl;
+            output << item.srItems << endl;
         }
 
 };

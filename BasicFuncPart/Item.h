aa0537fa21b89f66d6a87dc50d771d0088b0ee96
Zachary Handel <zachary.handelzh@gmail.com>
2022-09-08 21:16:15 -0500

    Initialize private and public members of item

---
+++ BasicFuncPart/Item.h
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class Item
+{
+    private:
+        string itemName;
+        string itemDescription;
+        int srItems;
+    public:
+        void setName(string)
+        {
+
+        }
+
+        void setDesc(string)
+        {
+
+        }
+
+        void setSR(int)
+        {
+
+        }
+
+        string getName()
+        {
+
+        }
+
+        string getDesc()
+        {
+
+        }
+
+        int getSR()
+        {
+
+        }
+
+        void displayName()
+        {
+
+        }
+
+        void displayDesc()
+        {
+            
+        }
+
+};

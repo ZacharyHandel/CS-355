2e34c11229d4b2ed0d97fa7725027bb7674e6e70
Zachary Handel <zachary.handelzh@gmail.com>
2022-09-19 15:42:20 -0500

    FInal Commit

---
+++ BasicFuncPart/BasicFuncPart/Area-Zachs_Laptop.h
@@ -0,0 +1,65 @@
+#ifndef AREA_H
+#define AREA_H
+
+#include <string>
+#include <iostream>
+#ifndef ullItem
+#define ullItem
+	#include "ull.h"
+	#include "Item.h"
+#endif
+
+using namespace std;
+
+class Area{
+  public:
+    void setDescription (string desc){
+    	description = desc;
+    }
+    void setGoal(bool g){
+	goal = g;		
+    }
+    void setID(bool id){
+	instadeath = id;		
+    }
+    string getDescription(){
+    	return description;
+    }
+    bool getID(){
+    	return instadeath;
+    }
+    bool getGoal(){
+	return goal;
+    }
+    void displayArea(){
+	cout<<description<<endl;		
+    }
+    
+    uLList<Item*> items;
+
+    void search()
+    {
+        if(items.isEmptyList())
+        {
+            cout << "No items in this area." << endl;
+        }
+        else
+        {
+            nodeType<Item*> *tempPointer = items.getFirst();
+            cout << "The following items are in this area: " << endl;
+            while(tempPointer != NULL)
+            {
+                cout << "\t" << tempPointer->info->getName() << endl;
+                tempPointer = tempPointer -> link;
+            }
+            
+        }
+    }
+    
+  private:
+     string description;
+     bool instadeath;
+     bool goal;	
+};
+
+#endif
--- BasicFuncPart/BasicFuncPart/Map-Zachs_Laptop.h
+++ BasicFuncPart/BasicFuncPart/Map-Zachs_Laptop.h
@@ -5,10 +5,13 @@
 #include <sstream> //??
 #include <stdlib.h>
 
-
+#ifndef MAN_H
+#define MAP_H
 #include "IFDParser.h"
 #include "LinkData.h"
 #include "Area.h"
+//#include "Item.h"
+#endif
 
 using namespace std;
 
@@ -22,7 +25,7 @@ struct areaNode
 };
 
 class Map{
-	private:
+	protected:
 		vector<areaNode*> areasVec;
 		int areacnt;
 
@@ -36,7 +39,7 @@ class Map{
 		bool ifderr;
 
 	public:
-		Map();
+		//Map();
 
 		void makeArea(){
 			int id;
